8127f5d3913c04fd7775c44f287fb44d6e718846
Konrad Buchmann <konrad.buchmann@icloud.com>
2022-02-01 18:27:24 +0100

    forward algorithm + sensitivity, specificity for viterbi

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,7 @@ enum State {
 // Fair   = 'F'
 // Loaded = 'L'
 impl fmt::Display for State {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self {
             State::Fair => write!(f, "F"),
             State::Loaded => write!(f, "L"),
@@ -19,6 +19,9 @@ impl fmt::Display for State {
     }
 }
 
+// This would usually be called a DP-Matrix,
+// but I find it easier to think of it as a graph.
+// Sort of like a HMM extended by an additional time dimension
 struct ViterbiGraphNode {
     back_ref: Option<State>, // back-reference to the most probable previous state
     prob: f64,               // probability of the current state
@@ -31,7 +34,12 @@ struct HiddenMarkovModel {
 }
 
 impl HiddenMarkovModel {
+
+    // Finds the most probable sequence of states for a given sequence of observations, 
+    // using the Viterbi algorithm.
+    // Returns a tuple containing the state sequence, and it's probability.
     fn viterbi(&self, observations: &[u8], initial_p: HashMap<State, f64>) -> (Vec<State>, f64) {
+        // Initialize an empty Viterbi Graph
         let mut graph: HashMap<State, Vec<ViterbiGraphNode>> = HashMap::new();
 
         for state in &self.states {
@@ -90,12 +98,55 @@ impl HiddenMarkovModel {
 
         (final_path, start.prob)
     }
+
+    // Calculate the probability of a path going through a certain node, for all nodes,
+    // using the Forward-Algorithm.
+    // This is essentially the Viterbi algorithm without back-references and
+    // using sum instead of max.
+    // Returns the graph containing all the probabilities, 
+    // and a final probability for the entire observed sequence
+    fn forward(&self, observations: &[u8], initial_p: HashMap<State, f64>) -> (HashMap<State, Vec<f64>>, f64) {
+        let mut graph: HashMap<State, Vec<f64>> = HashMap::new();
+        
+        for state in &self.states {
+            graph.insert(*state, Vec::with_capacity(observations.len()));
+        }
+
+        // initialize nodes for the first observation
+        // using the provided initial state probabilities
+        for (state, prob) in initial_p {
+            graph.get_mut(&state)
+                .unwrap()
+                .push(prob * self.emission_p[&state][&observations[0]]);
+        }
+
+        for (i, obs) in observations.iter().enumerate().skip(1) {
+            for current_state in &self.states {
+                let mut sum = 0.0;
+
+                for prev_state in &self.states {
+                    sum += graph[current_state][i-1] * self.transition_p[&(*prev_state, *current_state)];
+                }
+
+                graph.get_mut(current_state)
+                    .unwrap()
+                    .push(sum * self.emission_p[current_state][obs]);
+            }
+        }
+
+        let total_p: f64 = self.states.iter()
+            .map(|s| *graph[s].last().unwrap())
+            .sum();
+
+        (graph, total_p)
+    }
 }
 
 fn main() {
-    let rolls = std::fs::read_to_string("data/Casino.txt").unwrap();
+    use State::*;
 
-    let rolls: Vec<u8> = rolls
+    let rolls: Vec<u8> = std::fs::read_to_string("data/Casino.txt")
+        .unwrap()
         .chars()
         .map(|c| c.to_digit(10).unwrap() as u8)
         .collect();
@@ -103,7 +154,7 @@ fn main() {
     assert_eq!(rolls.len(), 300);
 
 
-    use State::*;
+    // === Define the Hidden Markov Model ===
 
     let transition_p = HashMap::from([
         ((Fair,   Fair),   0.95),
@@ -113,20 +164,11 @@ fn main() {
     ]);
 
     let emission_p = HashMap::from([
-        ( 
-            Fair, 
-            HashMap::from(
-                [(1, 1./6.), (2, 1./6.), (3, 1./6.), (4, 1./6.), (5, 1./6.), (6, 1./6.)]
-            ) 
-        ),
-        ( 
-            Loaded, 
-            HashMap::from(
-                [(1, 0.1  ), (2, 0.1  ), (3, 0.1  ), (4, 0.1  ), (5, 0.1  ), (6, 0.5  )]
-            ) 
-        )
+        ( Fair,   HashMap::from([(1, 1./6.), (2, 1./6.), (3, 1./6.), (4, 1./6.), (5, 1./6.), (6, 1./6.)]) ),
+        ( Loaded, HashMap::from([(1, 0.1  ), (2, 0.1  ), (3, 0.1  ), (4, 0.1  ), (5, 0.1  ), (6, 0.5  )]) )
     ]);
 
+    // Let's start fair
     let initial_p = HashMap::from([
         (Fair,   1.0),
         (Loaded, 0.0)
@@ -135,10 +177,48 @@ fn main() {
     let states = vec![Fair, Loaded];
 
     let hmm = HiddenMarkovModel { transition_p, emission_p, states };
-    let (best_path, best_prob) = hmm.viterbi(&rolls, initial_p);
 
-    println!("Most probable path with probability {:e}:", best_prob);
-    for s in best_path {
+
+    // === Run the Viterbi Algorithm, and calculate the TPR and TNR ===
+    let (viterbi_path, path_prob) = &hmm.viterbi(&rolls, initial_p);
+
+    println!(" === Viterbi-Algorithm ===");
+    println!("Most probable path with probability {:.4e}:", path_prob);
+    for s in viterbi_path {
         print!("{s}");
     }
+    println!();
+
+    // TPR (sensitivity), TNR (specificity)
+    // calculated using the true states that generated the given observations (Durbin)
+    let ground_truth: Vec<State> = std::fs::read_to_string("data/ground_truth.txt")
+        .unwrap()
+        .chars()
+        .map(|c| match c {
+            'F' => Fair,
+            'L' => Loaded,
+            _   => panic!(),
+        })
+        .collect();
+    
+    let tp = ground_truth
+        .iter()
+        .zip(viterbi_path.iter())
+        .filter(|x| x == &(&Loaded, &Loaded))
+        .count();
+    
+    let tn = ground_truth
+        .iter()
+        .zip(viterbi_path.iter())
+        .filter(|x| x == &(&Fair, &Fair))
+        .count();
+
+    let p = ground_truth.iter().filter(|x| **x == Loaded).count();
+    let n = ground_truth.iter().filter(|x| **x == Fair  ).count();
+
+    let tpr = tp as f64 / p as f64;
+    let tnr = tn as f64 / n as f64;
+
+    println!("Sensitivitvity: {:.1}%, Specificity: {:.2}%", tpr * 100., tnr * 100.);
+
 }
